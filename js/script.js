517245f4528a67122d929ffc2e2bedfc8ce6077b
Gioele Farina <jchilosa@gmail.com>
2020-12-02 17:42:49 +0100

    bk

--- js/script.js
+++ js/script.js
@@ -33,7 +33,6 @@ var app = new Vue({
     },
 
     getMovies: function(){
-
       /*
       esempio richiesta
       https://api.themoviedb.org/3/search/movie?api_key=cd471903e138fa6aad2b7a7c8910d06f&language=it-IT&query=casa
@@ -44,8 +43,35 @@ var app = new Vue({
       .then(risposta => {
         console.log("Totale risultati: ", risposta.data.total_results);
         this.movies = risposta.data.results;
+        console.log("Array risultati:", this.movies);
       });
-    }
+    },
 
+    getMovieDetails: function(movieID, datoRichiesto){
+      /*
+      esempio di richiesta
+      https://api.themoviedb.org/3/movie/741074?api_key=cd471903e138fa6aad2b7a7c8910d06f
+      */
+      let queryDettagli = `${this.apiDettagliFilm}${movieID}?api_key=${this.apiKey}`;
+
+      axios.get(queryDettagli)
+      .then(risposta => {
+        // ritorna le lingue sotto forma di stringa
+        if (datoRichiesto === "spoken_languages") {
+          let lingue = "";
+          risposta.data.spoken_languages.forEach((item, i) => {
+            lingue += item.name + " ";
+          });
+          console.log(lingue);
+          return lingue;
+        // ritorna il dato richiesto
+        } else {
+          console.log(risposta.data[datoRichiesto]);
+          return risposta.data[datoRichiesto];
+        }
+
+      });
+
+    }
   }
 })
