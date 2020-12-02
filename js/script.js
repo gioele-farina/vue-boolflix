09cb98da0053e6db33ff9fe187581ade16d92f97
Gioele Farina <jchilosa@gmail.com>
2020-12-02 16:23:26 +0100

    bozza interrogazione api

--- js/script.js
+++ js/script.js
@@ -23,19 +23,28 @@ var app = new Vue({
 
     avviaRicerca: function(){
       if (this.ricerca !== "") {
-        console.log(this.ricerca);
+        // l'api vuole "+" anzichè gli spazi, quindi li converto
+        this.query = this.ricerca.replace(/ /g, "+");
+        console.log("Ricerca: ", this.query);
         this.ricerca = "";
+
+        this.getMovies();
       }
     },
 
     getMovies: function(){
 
-/*
-      axios.get('')
-      .then(risposta => {
+      /*
+      esempio richiesta
+      https://api.themoviedb.org/3/search/movie?api_key=cd471903e138fa6aad2b7a7c8910d06f&language=it-IT&query=casa
+      */
+      let queryCompleta = `${this.apiFilm}api_key=${this.apiKey}&language=${this.language}&query=${this.query}`;
 
+      axios.get(queryCompleta)
+      .then(risposta => {
+        console.log(risposta);
       });
-*/
+
     }
 
   }
