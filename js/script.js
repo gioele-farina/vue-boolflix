66db343bd88e3f24592a283d4be9c4dc4270d6c0
Gioele Farina <jchilosa@gmail.com>
2020-12-03 11:30:34 +0100

    milestone1 refactoring

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ var app = new Vue({
   mounted: function () {
   this.$nextTick(function () {
 
-
   })},
 
   computed: {
@@ -37,23 +36,26 @@ var app = new Vue({
         console.log("Ricerca: ", this.query);
         this.ricerca = "";
 
-        // Interrogo l'Api
+        // Interrogo l'Api (movies, series ecc)
         this.getMovies();
       }
     },
 
     getMovies: function(page){
-      /*
-      esempio richiesta
-      https://api.themoviedb.org/3/search/movie?api_key=cd471903e138fa6aad2b7a7c8910d06f&language=it-IT&query=casa
-      */
+
+      // Se il parametro pagina non è specificato, mette 1 di default
       if (isNaN(page)) {
         page = 1;
       }
 
-      let queryCompleta = `${this.apiFilm}api_key=${this.apiKey}&language=${this.language}&query=${this.query}&page=${page}`;
-
-      axios.get(queryCompleta)
+      axios.get(this.apiFilm, {
+      params: {
+        api_key: this.apiKey,
+        language: this.language,
+        query: this.query,
+        page: page
+      }
+      })
       .then(risposta => {
         console.log("Totale risultati: ", risposta.data.total_results);
         console.log("Pagine totali: ", risposta.data.total_pages);
@@ -62,13 +64,11 @@ var app = new Vue({
         this.movies = risposta.data.results;
         this.pagineTotali = risposta.data.total_pages;
         this.paginaAttuale = risposta.data.page;
-        // console.log("Array risultati:", this.movies);
-
-        // this.getMoviesMoreInfo(); il dom viene aggiornato ad ogni singola iterazione del for each
       });
     },
 
     getMoviesMoreInfo: function(){
+      // DA MODIFICARE AL MOMENTO NON ATTIVA
       // appende le informazioni aggiuntive
       /*
       esempio di richiesta
@@ -113,39 +113,3 @@ var app = new Vue({
 
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-/*
-    getMovieDetails: function(movieID, datoRichiesto){
-      esempio di richiesta
-      https://api.themoviedb.org/3/movie/741074?api_key=cd471903e138fa6aad2b7a7c8910d06f
-      let queryDettagli = `${this.apiDettagliFilm}${movieID}?api_key=${this.apiKey}`;
-
-      axios.get(queryDettagli)
-      .then(risposta => {
-        // ritorna le lingue sotto forma di stringa
-        if (datoRichiesto === "spoken_languages") {
-          let lingue = "";
-          risposta.data.spoken_languages.forEach((item, i) => {
-            lingue += item.name + " ";
-          });
-          console.log(lingue);
-          return lingue;
-        // ritorna il dato richiesto
-        } else {
-          console.log(risposta.data[datoRichiesto]);
-          return risposta.data[datoRichiesto];
-        }
-
-      });
-    },
-*/
